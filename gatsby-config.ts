84226203e75893e610a7b6444f140bd9251cbd52
Gustavo Simões <gustavo.simoes@grad.ufsc.br>
2023-03-22 01:46:09 -0300

    feat: Melhorias SEO
    Signed-off-by: Gustavo Simões <gustavo.simoes@grad.ufsc.br>

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,9 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `JOFEESC`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://jofeesc.ufsc.br`,
+    description: `I Jornada de Fisioterapia Esportiva do Extremo Sul Catarinense`,
+    image: `/logo.png`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -18,8 +20,7 @@ const config: GatsbyConfig = {
       options: {
         // Setting a color is optional.
         color: `#00C2CB`,
-        // Disable the loading spinner.
-        showSpinner: false,
+
       },
     },
     {
